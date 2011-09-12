7988a9d5eb38913098da7265b858fd32e55dc158
Francois Keith <francois.keith@inrialpes.fr>
2011-09-12 12:20:17 +0200

    Initialize properly m_AnkleSoilDistance

--- src/pg.cpp
+++ src/pg.cpp
@@ -171,10 +171,7 @@ typeName & PatternGenerator::functionName(typeName & res, int /*time*/)	 const	\
       m_SupportFoot = 1; // Means that we do not know which support foot it is.
       m_ReferenceFrame = WORLD_FRAME;
 
-      /* TODO: To be removed and extract from
-	 Humanoid specificities.
-      */
-      m_AnkleSoilDistance = 0.105;
+      m_AnkleSoilDistance = 0.0; // This value is initialized later.
       sotDEBUGIN(5);
 
       firstSINTERN.setDependencyType(TimeDependency<int>::BOOL_DEPENDENT);
@@ -404,6 +401,11 @@ typeName & PatternGenerator::functionName(typeName & res, int /*time*/)	 const	\
       // Parsing the file.
       string RobotFileName = m_vrmlDirectory + m_vrmlMainFile;
       dynamicsJRLJapan::parseOpenHRPVRMLFile(*aHDR,RobotFileName,m_xmlRankFile,m_xmlSpecificitiesFile);
+
+      // Initialize m_AnkleSoilDistance
+      vector3d ankle;
+      aHDR->leftFoot()->getAnklePositionInLocalFrame(ankle);
+      m_AnkleSoilDistance = ankle[2];
 
       try
 	{
